1652c57c30270081acd1a30c73d485aca6b55a5b
oren <get.oren@gmail.com>
2015-01-05 08:10:34 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$( document ).ready(function() {
-    $('#s454-gregorian').timepicker();
+$(document).ready(function() {
+    $('#s454-gregorian').datepicker();
     $('#s454-gregorian').change(function(){
       $('#s454-symmetry454').val((new Date($(this).val())).toSymmetry454String());
     });
