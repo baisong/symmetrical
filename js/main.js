cba288e64829898690f27f2a79e296acffd435aa
oren <get.oren@gmail.com>
2015-01-05 08:08:31 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$(function(){
-  console.log('Hello calendar!');
-})(jQuery);
+$( document ).ready(function() {
+    $('#s454-gregorian').timepicker();
+    $('#s454-gregorian').change(function(){
+      $('#s454-symmetry454').val((new Date($(this).val())).toSymmetry454String());
+    });
+});
